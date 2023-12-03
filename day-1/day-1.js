f564833c1734e785d6c008b206c63f5a24082d32
Michal Iwanski <michalronin@outlook.com>
2023-12-03 13:49:23 +0100

    Day 1, part 2 done

--- day-1/day-1.js
+++ day-1/day-1.js
@@ -13,15 +13,23 @@ function removeLetters(arr) {
   return arr.map(item => item.replace(/\D/g, ''));
 }
 
-export function firstAndLastNumbers(arr) {
-  return arr.map(item => {
-    let numbers = item.replace(/\D/g, '');
-    let first = numbers.charAt(0);
-    let last = numbers.charAt(numbers.length - 1);
-    if (numbers.length === 1) {
-      last = first;
+const numberWords = {
+    'one': 1, 'two': 2, 'three': 3, 'four': 4, 
+    'five': 5, 'six': 6, 'seven': 7, 'eight': 8, 'nine': 9
+};
+
+export function extractNumbers(str) {
+    const numberWordsRegex = 'one|two|three|four|five|six|seven|eight|nine';
+    const regexPattern = new RegExp(`(?=(${numberWordsRegex}|\\d)).*(${numberWordsRegex}|\\d)`, 'i');
+    const matches = str.match(regexPattern);
+
+    if (matches && matches.length >= 3) {
+        let firstNum = isNaN(matches[1]) ? numberWords[matches[1].toLowerCase()] : parseInt(matches[1], 10);
+        let lastNum = isNaN(matches[2]) ? numberWords[matches[2].toLowerCase()] : parseInt(matches[2], 10);
+
+        return `${firstNum}${lastNum}`;
     }
 
-    return first + last;
-  });
+    return null;
 }
+
